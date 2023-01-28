96f8764658d0eff35df66b7bac26b1aa79689014
zakaria-akash <zakaria.93@yahoo.com>
2023-01-29 03:05:00 +0600

    modifying acrt-context to increase the product quantity only when adding existing product to the cart

--- store/cart-context.js
+++ store/cart-context.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createContext } from "react";
 
 const CartContext = createContext({
@@ -11,6 +11,13 @@ export const CartContextProvider = (props) => {
   const [activeCartItems, setActiveCartItems] = useState([]);
 
   const addItemToCartHandler = (dataToAdd) => {
+    activeCartItems.map((item) => {
+      if (item.id == dataToAdd.id) {
+        dataToAdd.quantity =
+          parseInt(item.quantity) + parseInt(dataToAdd.quantity);
+        setActiveCartItems(activeCartItems.filter((itm) => itm.id !== item.id));
+      }
+    });
     setActiveCartItems((prevData) => [...prevData, dataToAdd]);
   };
 
